45ff6a0053d6b59651f0ae09429b499452ea3f99
Luka <pablolucasso@icloud.com>
2022-09-27 18:22:05 +0200

    Logo img

--- components/Logo/index.js
+++ components/Logo/index.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import { useRouter } from 'next/router'
+import Image from 'next/image'
+
+import logo from './../../public/images/jugometal-logo.png'
 
 const Logo = ({ cls }) => {
   const router = useRouter()
 
   return (
-    <div className={`logo ${cls && cls}`} onClick={() => router.replace('/')} />
+    <div className={`logo ${cls && cls}`} onClick={() => router.replace('/')}>
+      <Image
+        src={logo}
+        alt={'Jugometal Logo'}
+        layout={'fill'}
+        objectFit={'contain'}
+        objectPosition='center'
+        style={{ zIndex: '-10' }}
+      />
+    </div>
   )
 }
 
