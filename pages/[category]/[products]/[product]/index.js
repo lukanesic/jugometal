70174a134c5d0e689931a4290b8e97d101aec493
Luka <pablolucasso@icloud.com>
2022-11-22 13:34:57 +0100

    fallback true

--- pages/[category]/[products]/[product]/index.js
+++ pages/[category]/[products]/[product]/index.js
@@ -183,7 +183,7 @@ export async function getStaticPaths() {
   })
   return {
     paths: paths,
-    fallback: false,
+    fallback: true,
   }
 }
 
--- pages/[category]/index.js
+++ pages/[category]/index.js
@@ -87,7 +87,7 @@ export const getStaticPaths = async () => {
   })
   return {
     paths: paths,
-    fallback: false,
+    fallback: true,
   }
 }
 
