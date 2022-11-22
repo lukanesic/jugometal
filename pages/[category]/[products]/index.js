cef36a90fcc9a2ef2d317de1e4365794c6da6f65
Luka <pablolucasso@icloud.com>
2022-11-22 13:39:21 +0100

    fix err

--- pages/[category]/[products]/index.js
+++ pages/[category]/[products]/index.js
@@ -103,7 +103,7 @@ export const getStaticPaths = async () => {
   })
   return {
     paths: paths,
-    fallback: false,
+    fallback: true,
   }
 }
 
--- pages/[category]/index.js
+++ pages/[category]/index.js
@@ -87,7 +87,7 @@ export const getStaticPaths = async () => {
   })
   return {
     paths: paths,
-    fallback: true,
+    fallback: false,
   }
 }
 
